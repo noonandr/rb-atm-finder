37e30260c44cf44fc9f42a31fdef81b1302e17b4
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-06-03 16:16:58 +0100

    feat(atm-service); #RB-003 added base for atm finder with sorting by latitude

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,47 @@
 <template>
   <div id="app">
+
+    <button v-on:click="get_atms">Get atms</button>
+
+    <div v-if="loading">
+      Loading...
+    </div>
+ 
+    <div v-for="(atm, index) in latSortedAtms" :key="atm.id" v-if="index <= 4">
+
+        <p>{{ atm.Location.PostalAddress.AddressLine[0] }}</p>
+        <p>{{ atm.Location.PostalAddress.Country }}</p>
+        <br>
+
+    </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import axios from 'axios';
+  
+  export default {
+    name: 'app',
+    data () {
+      return {
+        latSortedAtms: [],
+        loading: false
+      }
+    }, 
+    methods: {
+      get_atms: function () {
+         return axios("https://api.lloydsbank.com/open-banking/v2.1/atms", {
+          method: 'GET'
+        }).then(response => {
+          this.loading = false;
+          let atms = response.data.data[0].Brand[0].ATM;
+          this.latSortedAtms = atms.sort(function(a, b){
+            return a.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude > b.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude;
+          })
+        })
+      }
+    }
   }
-}
 </script>
 
 <style>
