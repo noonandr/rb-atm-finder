e2aad457ffe0f0040620bcde3141a681435b11c3
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-06-03 20:29:56 +0100

    feat(list): #RB-004 added layout for list items

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <template>
   <div id="app">
 
-    <div v-if="loading">
-      Loading...
-    </div>
+    <div class="page-width">
 
-    <div v-for="(atm, index) in orderedAtms" :key="atm.id" v-if="index <= 10">
+      <div class="feed">
 
-      <p>{{ atm.Location.PostalAddress.AddressLine[0] }}</p>
-      <p>{{ atm.Location.PostalAddress.Country }}</p>
-      <p>{{ atm.distance }} Km</p>
-      <p><a v-bind:href="atm.url" target="_blank">show on map</a></p>
-      <br>
+        <div class="title">Your closest 10 ATM locations</div>
 
+        <div v-if="loading">
+          Loading...
+        </div>
+
+        <div v-for="(atm, index) in orderedAtms" :key="atm.id" v-if="index < 10">
+
+          <div class="item">
+            <div class="details">
+              <div class="bank"><span class="label">Bank </span><span class="value">Lloyds Bank</span></div>
+              <div class="country"><span class="label">Country </span><span class="value">{{ atm.Location.PostalAddress.Country }}</span></div>
+              <div class="address"><span class="label">Address </span><span class="value">{{ atm.Location.PostalAddress.AddressLine[0] }}</span></div>
+              <div class="distance"><span class="label">Distance </span><span class="value">{{ atm.distance }} Km</span></div>
+            </div>
+            <div class="geo-location">
+              <a class="link" v-bind:href="atm.url" target="_blank"><img src="./assets/pin.png"></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -73,7 +87,7 @@
               let distance = earthsRadius * angularDistance;
 
               // sets ATMs distance
-              atm.distance = distance;
+              atm.distance = distance.toFixed(2);
 
               // sets ATMs Google Maps API
               atm.url = 'https://www.google.com/maps/search/?api=1&query=' + atmLatitude + ',' + atmLongitude;
@@ -107,6 +121,229 @@
   }
 </script>
 
+
+
+
 <style>
+
+body {
+  background: #FAFAFA;
+  font-family: Helvetica, Arial, Sans-Serif;
+  color: #1c242b;
+}
+
+.page-width {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 12px;
+
+    @media (min-width: $screen-small) {
+        padding: 0 u(2);
+    }
+}
+
+.feed {
+  padding: 8px 12px;
+  background: #FFFFFF;
+}
+
+.title {
+  padding: 18px 0 24px;
+  text-align: center;
+  font-size: 24px;
+}
+
+.item {
+  padding: 18px;
+  display: flex;
+  border-top: 2px solid #FAFAFA;
+
+  @media (min-width: 480px) {
+    padding: 18px 8px;
+  }
+}
+
+.details {
+  flex: 1 1 auto;
+}
+
+.geo-location {
+  display: flex;
+  flex: 0 1 auto;
+  align-items: center;
+}
+
+.link img {
+  width: 28px;
+  padding: 0 6px;
+}
+
+.label {
+  width: 80px;
+  color: #1EAEDB;
+  font-weight: 200;
+}
+
+.bank {
+  display: flex;
+}
+
+.country {
+  display: flex;
+}
+
+.address {
+  display: flex;
+}
+
+.distance {
+  display: flex;
+}
+
+
+// -- RESET.CSS
+
+html {
+    overflow-x: hidden;
+    height: 100%;
+    margin: 0 auto;
+    font-size: 100%;
+    box-sizing: border-box;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+}
+
+* {
+    box-sizing: inherit;
+
+    ::before,
+    ::after {
+        box-sizing: inherit;
+    }
+}
+
+
+// -- body
+
+body {
+    overflow: show; // bricks reset legacy  (@todo investigate dropping this)
+    position: relative; // bricks reset legacy  (@todo investigate dropping this)
+    margin: 0;
+    font: 16px/1 sans-serif;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+}
+
+// -- typography
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+figure,
+ol,
+dl,
+dt,
+dd,
+ul {
+    margin: 0;
+    padding: 0;
+}
+
+main {
+    display: block;
+}
+
+ul,
+ol {
+    display: block;
+    list-style-type: none;
+}
+
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+    font-size: inherit;
+}
+
+a {
+    text-decoration: none;
+
+    &:hover,
+    &:active {
+        outline: 0;
+    }
+}
+
+input,
+textarea,
+keygen,
+select,
+button {
+    overflow: hidden;
+    font: inherit;
+    font-family: inherit;
+    letter-spacing: inherit;
+    -webkit-font-smoothing: inherit;
+}
+
+button  {
+    vertical-align: middle;
+    border: 0;
+    border-style: solid;
+    -webkit-appearance: button;
+}
+
+select {
+    overflow: auto;
+    border-radius: 0;
+}
+
+input {
+
+    &[type="button"],
+    &[type="reset"],
+    &[type="submit"] {
+        vertical-align: middle;
+        border: 0;
+        border-style: solid;
+        -webkit-appearance: button;
+    }
+
+    &[type="number"] {
+        -moz-appearance: textfield;
+
+        &::-webkit-inner-spin-button,
+        &::-webkit-outer-spin-button {
+            margin: 0;
+            -webkit-appearance: none;
+        }
+    }
+}
+
+:focus {
+    outline: 0;
+}
+
+
+img {
+    overflow-x: hidden;
+    vertical-align: middle;
+    max-width: 100%;
+    height: auto;
+    border: 0;
+}
+
+table {
+    border-collapse: collapse;
+    border-spacing: 0;
+}
 
 </style>
