fcb9ae9189b5829bb9f994641906bee5d416ccd4
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-06-03 20:29:56 +0100

    feat(atm-service); #RB-004 added link to open map location in new tab

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,12 @@
       Loading...
     </div>
 
-    <div v-for="(atm, index) in latSortedAtms" :key="atm.id" v-if="index <= 10">
+    <div v-for="(atm, index) in orderedAtms" :key="atm.id" v-if="index <= 10">
 
       <p>{{ atm.Location.PostalAddress.AddressLine[0] }}</p>
       <p>{{ atm.Location.PostalAddress.Country }}</p>
       <p>{{ atm.distance }} Km</p>
+      <p><a v-bind:href="atm.url" target="_blank">show on map</a></p>
       <br>
 
     </div>
@@ -23,7 +24,7 @@
     name: 'app',
     data () {
       return {
-        latSortedAtms: [],
+        orderedAtms: [],
         loading: false
       }
     },
@@ -34,6 +35,7 @@
         }).then(response => {
           this.loading = true;
 
+          // promise to make location accessible to distance calculation 
           var getPosition = function (options) {
             return new Promise(function (resolve, reject) {
               navigator.geolocation.getCurrentPosition(resolve, reject, options);
@@ -42,16 +44,22 @@
 
           getPosition()
           .then((position) => {
+            // sets visitors latitude and longitude
             const visitorLatitude = position.coords.latitude;
             const visitorLongitude = position.coords.longitude;
 
-            let atms = [];
+            // ATM array from response
             let allAtms = response.data.data[0].Brand[0].ATM;
+
+            let atms = [];
+
             for (var ix = 0; ix < allAtms.length; ix++) {
+              // atm
               let atm = allAtms[ix];
               let atmLatitude = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude;
               let atmLongitude = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates.Longitude;
 
+              // distance calculation
               const earthsRadius = 6371;
               let radsLatitude = (atmLatitude - visitorLatitude) * Math.PI / 180;
               let radsLongitude = (atmLongitude - visitorLongitude) * Math.PI / 180;
@@ -64,7 +72,13 @@
 
               let distance = earthsRadius * angularDistance;
 
+              // sets ATMs distance
               atm.distance = distance;
+
+              // sets ATMs Google Maps API
+              atm.url = 'https://www.google.com/maps/search/?api=1&query=' + atmLatitude + ',' + atmLongitude;
+
+              // push updated ATM objection into new array
               atms.push(atm);
             }
 
@@ -76,9 +90,9 @@
               return 0;
             }
 
-            this.latSortedAtms = atms.sort(sortDistance);
+            this.orderedAtms = atms.sort(sortDistance);
 
-
+            //  disables loading state
             this.loading = false;
 
           })
@@ -87,6 +101,7 @@
       }
     },
     beforeMount(){
+      // run main function on load
       this.get_atms()
     },
   }
