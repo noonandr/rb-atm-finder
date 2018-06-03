c57ca024219ebe1dd9f0ced9ad803165771cbe32
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-06-03 19:34:23 +0100

    feat(atm-service); #RB-003 updated main vue file to conclude key features of service

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
 
-    <button v-on:click="get_atms">Get atms</button>
-
     <div v-if="loading">
       Loading...
     </div>
- 
-    <div v-for="(atm, index) in latSortedAtms" :key="atm.id" v-if="index <= 4">
 
-        <p>{{ atm.Location.PostalAddress.AddressLine[0] }}</p>
-        <p>{{ atm.Location.PostalAddress.Country }}</p>
-        <br>
+    <div v-for="(atm, index) in latSortedAtms" :key="atm.id" v-if="index <= 10">
+
+      <p>{{ atm.Location.PostalAddress.AddressLine[0] }}</p>
+      <p>{{ atm.Location.PostalAddress.Country }}</p>
+      <p>{{ atm.distance }} Km</p>
+      <br>
 
     </div>
   </div>
@@ -19,7 +18,7 @@
 
 <script>
   import axios from 'axios';
-  
+
   export default {
     name: 'app',
     data () {
@@ -27,20 +26,69 @@
         latSortedAtms: [],
         loading: false
       }
-    }, 
+    },
     methods: {
       get_atms: function () {
-         return axios("https://api.lloydsbank.com/open-banking/v2.1/atms", {
+        return axios("https://api.lloydsbank.com/open-banking/v2.1/atms", {
           method: 'GET'
         }).then(response => {
-          this.loading = false;
-          let atms = response.data.data[0].Brand[0].ATM;
-          this.latSortedAtms = atms.sort(function(a, b){
-            return a.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude > b.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude;
+          this.loading = true;
+
+          var getPosition = function (options) {
+            return new Promise(function (resolve, reject) {
+              navigator.geolocation.getCurrentPosition(resolve, reject, options);
+            });
+          }
+
+          getPosition()
+          .then((position) => {
+            const visitorLatitude = position.coords.latitude;
+            const visitorLongitude = position.coords.longitude;
+
+            let atms = [];
+            let allAtms = response.data.data[0].Brand[0].ATM;
+            for (var ix = 0; ix < allAtms.length; ix++) {
+              let atm = allAtms[ix];
+              let atmLatitude = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates.Latitude;
+              let atmLongitude = atm.Location.PostalAddress.GeoLocation.GeographicCoordinates.Longitude;
+
+              const earthsRadius = 6371;
+              let radsLatitude = (atmLatitude - visitorLatitude) * Math.PI / 180;
+              let radsLongitude = (atmLongitude - visitorLongitude) * Math.PI / 180;
+
+              var chordLength = Math.sin(radsLatitude / 2) * Math.sin(radsLatitude / 2) +
+                Math.cos(visitorLatitude * Math.PI / 180) * Math.cos(atmLatitude * Math.PI / 180) *
+                Math.sin(radsLongitude / 2) * Math.sin(radsLongitude / 2);
+
+              var angularDistance = 2 * Math.atan2(Math.sqrt(chordLength), Math.sqrt(1 - chordLength));
+
+              let distance = earthsRadius * angularDistance;
+
+              atm.distance = distance;
+              atms.push(atm);
+            }
+
+            function sortDistance(a,b) {
+              if (a.distance < b.distance)
+                return -1;
+              if (a.distance > b.distance)
+                return 1;
+              return 0;
+            }
+
+            this.latSortedAtms = atms.sort(sortDistance);
+
+
+            this.loading = false;
+
           })
+
         })
       }
-    }
+    },
+    beforeMount(){
+      this.get_atms()
+    },
   }
 </script>
 
